7a52640e52d5fcb35b1fb4bfc7fcc5cd46d127fe
feelonewong <feelonewong@gmail.com>
2020-08-10 15:36:07 +0800

    navigation explain

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,10 @@
     <Topnav/>
     <div class="banner">
       <h1>ifeel design</h1>
-      <h2>一个历害的UI框架</h2>
+      <h2>一套基于 Vue3.0的 UI 组件库</h2>
       <p class="actions">
-        <a>Github</a>
-        <a>开始</a>
+        <a href="https://github.com/feelonewong/ifeel-design-vue" target="_blank">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -14,13 +14,42 @@
 
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
+import { provide, ref } from 'vue';
+
 export default {
   components:{
     Topnav
+  },
+  setup(){
+    const menuVisible = ref(false); 
+    provide('xxx',menuVisible);
+    return{
+      menuVisible
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
