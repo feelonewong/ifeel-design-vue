10e7c7e5e1683f98ed5f8793318a5d3a903fa8da
feelonewong <feelonewong@gmail.com>
2020-08-10 10:34:35 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+ <Ifeel/>
+</template>
+
+<script>
+import Ifeel from "./components/Ifeel.vue";
+export default {
+  name: 'App',
+  components:{
+    Ifeel
+  }
+}
+</script>
